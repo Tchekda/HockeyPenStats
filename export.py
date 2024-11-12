a5dbac7c847400882c343781f4e1d247424699ee
David Tchekachev <contact@tchekda.fr>
2024-11-12 01:45:39 +0100

    feat(date): remove miliseconds from format

--- export.py
+++ export.py
@@ -63,11 +63,11 @@ def get_game_penalties(base_url, game_id: int) -> str:
             "/".join(game_data['date_rencontre_non_formate'].split(' ')[0].replace('-', '/').split('/')[::-1]),
             game_data['receveur']['abreviation'],
             game_data['visiteur']['abreviation'],
-            f"{int(penalty['temps']/60)}:{penalty['temps']%60}:00",
+            f"{int(penalty['temps']/60)}:{penalty['temps']%60}",
             penalty['equipe']['abreviation'],
             penalty['joueur']['nom_complet'] if penalty['joueur'] else "",
             penalty['substitution']['nom_complet'] if penalty['substitution'] else "",
-            f"{penalty['temps_penalite'] if penalty['temps_penalite'] else 2}:00:00",
+            f"{penalty['temps_penalite'] if penalty['temps_penalite'] else 2}:00",
             penalty['sanction']['code'],
         ]
         formatted_penalties.append(formatted_penalty)
